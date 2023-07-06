c1864a22706a66d1f7917b38d32de27be2054d48
whalefall541 <jackchen541@sina.com>
2023-07-06 22:50:45 +0800

    degrade auto-service version

--- annotation-processor/src/main/java/com/whalefall/apt/MyAnnotationProcessor.java
+++ annotation-processor/src/main/java/com/whalefall/apt/MyAnnotationProcessor.java
@@ -33,13 +33,8 @@ public class MyAnnotationProcessor extends AbstractProcessor {
 
     @Override
     public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
-        messager.printMessage(Diagnostic.Kind.NOTE, "processor is processing");
-        annotations.forEach(e -> messager.printMessage(Diagnostic.Kind.NOTE, e.getQualifiedName()));
+        messager.printMessage(Diagnostic.Kind.NOTE, "processor is processing @ "+annotations+" @ "+roundEnv);
 
-        messager.printMessage(Diagnostic.Kind.NOTE, "separator");
-
-        Set<? extends Element> rootElements = roundEnv.getRootElements();
-        rootElements.forEach(r -> messager.printMessage(Diagnostic.Kind.NOTE, r.getSimpleName()));
         return true;
     }
 
