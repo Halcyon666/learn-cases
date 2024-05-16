b4e0f3c5e7cf3364fa309f70e25db7aa72c5c0e7
Halcyon666 <whalefallcl@gmail.com>
2024-05-17 00:25:05 +0800

    optimize code  CountDownLatch
    Signed-off-by: Halcyon666 <whalefallcl@gmail.com>

--- learncases/src/main/java/com/whalefall/learncases/future/LatchFuture.java
+++ learncases/src/main/java/com/whalefall/learncases/future/LatchFuture.java
@@ -1,6 +1,7 @@
 package com.whalefall.learncases.future;
 
 import jakarta.annotation.Nullable;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.util.Assert;
 
 import java.util.concurrent.*;
@@ -10,11 +11,13 @@ import java.util.concurrent.*;
  *
  * @since 1.0.0
  */
+@Slf4j
 public class LatchFuture implements Future<String> {
     private final CountDownLatch latch = new CountDownLatch(1);
     private volatile String msg;
 
     public void putMsgAndReleaseLock(String msg) {
+        log.info("lock countDown");
         this.msg = msg;
         latch.countDown();
     }
@@ -41,8 +44,9 @@ public class LatchFuture implements Future<String> {
     }
 
     @Override
-    public String get(long timeout, @Nullable TimeUnit unit) throws InterruptedException, ExecutionException, TimeoutException {
+    public String get(long timeout, @Nullable TimeUnit unit) throws InterruptedException, TimeoutException {
         Assert.notNull(unit, "TimeUnit must not be null");
+        // not get lock, timeout elapses
         if (!latch.await(timeout, unit)) {
             throw new TimeoutException("Timeout while waiting for result");
         }
--- learncases/src/test/java/com/whalefall/learncases/future/LatchFutureTests.java
+++ learncases/src/test/java/com/whalefall/learncases/future/LatchFutureTests.java
@@ -3,7 +3,6 @@ package com.whalefall.learncases.future;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
 
@@ -15,22 +14,23 @@ class LatchFutureTests {
     static final String MSG = "hello world from LatchFutureTests";
 
     @Test
-    void testFutureNotTimeOut() throws ExecutionException, InterruptedException, TimeoutException {
+    void testFutureNotTimeOut() throws InterruptedException, TimeoutException {
         final LatchFuture latchFuture = new LatchFuture();
-        new Thread(() -> {
-            try {
-                Thread.sleep(500); // Simulate some delay
-                latchFuture.putMsgAndReleaseLock(MSG);
-            } catch (InterruptedException e) {
-                Thread.currentThread().interrupt();
-            }
-        }).start();
+        latchFuture.putMsgAndReleaseLock(MSG);
         Assertions.assertEquals(MSG, latchFuture.get(1, TimeUnit.SECONDS));
     }
 
     @Test
     void testFutureTimeOut() {
         final LatchFuture latchFuture = new LatchFuture();
+        new Thread(() -> {
+            try {
+                Thread.sleep(5000); // Simulate some delay
+                latchFuture.putMsgAndReleaseLock(MSG);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        }).start();
         Assertions.assertThrows(TimeoutException.class, () -> latchFuture.get(1, TimeUnit.SECONDS));
     }
 }
