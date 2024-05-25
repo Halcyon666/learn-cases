9758e620bc9aa496146a76f909ecd7d9c5e5cd7e
Halcyon666 <whalefallcl@gmail.com>
2024-05-25 21:56:59 +0800

    add comment for SPI
    Signed-off-by: Halcyon666 <whalefallcl@gmail.com>

--- learncases/src/main/java/com/whalefall/learncases/LearncasesApplication.java
+++ learncases/src/main/java/com/whalefall/learncases/LearncasesApplication.java
@@ -6,6 +6,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+/**
+ * don't want to import the package, just put {@link LearncasesApplication} to
+ * {@code com.whalefall.LearncasesApplication}, auto scan all the classes
+ * in the sub-directroy {@code com.whalefall.LearncasesApplication} .
+ *
+ * <P>but change spi base directory to {@code com.whalefall} does not work.
+ *
+ * <p>Maybe some better way to do this.
+ */
 @SpringBootApplication
 @EnableScheduling
 @Import(ScanPackageConfig.class)
