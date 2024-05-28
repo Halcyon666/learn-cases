8a58267f734a2829e07235248d29dc7637ea8939
Halcyon666 <whalefallcl@gmail.com>
2024-05-29 00:04:30 +0800

    mapstruct mapping field rule
    Signed-off-by: Halcyon666 <whalefallcl@gmail.com>

---
+++ learncases/src/main/java/com/whalefall/learncases/mapstruct/MapStructEntity1.java
@@ -0,0 +1,22 @@
+package com.whalefall.learncases.mapstruct;
+
+import lombok.Builder;
+import lombok.Data;
+
+/**
+ * @author Halcyon
+ * @date 2024/5/28 23:28
+ * @since 1.0.0
+ */
+@Data
+@Builder
+public class MapStructEntity1 {
+    private String xAdress;
+    private String aAdress;
+    private String sMSG;
+    private String sMsgHello;
+    private String smSgHello1;
+    private String name;
+    private int age;
+    private String sSr;
+}
