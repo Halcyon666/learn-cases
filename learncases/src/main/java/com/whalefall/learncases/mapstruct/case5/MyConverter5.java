f5305a1286c6cb629134c7c627db8da736356473
Halcyon666 <whalefallcl@gmail.com>
2024-06-03 21:23:46 +0800

    Close #5 - Future CompletableFuture done   besides add jacoco coverage
    Signed-off-by: Halcyon666 <whalefallcl@gmail.com>

--- learncases/src/main/java/com/whalefall/learncases/mapstruct/case5/MyConverter5.java
+++ learncases/src/main/java/com/whalefall/learncases/mapstruct/case5/MyConverter5.java
@@ -11,7 +11,6 @@ import org.springframework.util.StringUtils;
  * @date 2024/5/29 21:03
  * @since 1.0.0
  */
-@SuppressWarnings("all")
 @Mapper(unmappedTargetPolicy = ReportingPolicy.ERROR)
 public interface MyConverter5 {
     MyConverter5 INSTANCE = org.mapstruct.factory.Mappers.getMapper(MyConverter5.class);
@@ -21,24 +20,24 @@ public interface MyConverter5 {
         return Status.getStatus(status);
     }
 
-    @SuppressWarnings("unused")
     @Mapping(target = "status", source = "entity3.status", qualifiedByName = "getStatus")
     MapStructEntity4 convert(MapStructEntity3 entity3, String haha);
 
+    @SuppressWarnings("unused")
     @BeforeMapping
     default void beforeMapping(@MappingTarget MapStructEntity4 entity4, MapStructEntity3 entity3) {
         if (entity3 == null) {
             return;
         }
         // if entity3 is null, then fill it with default value A
-        if (StringUtils.isEmpty(entity3.getStatus())) {
+        if (StringUtils.hasText(entity3.getStatus())) {
             entity3.setStatus("active");
         }
     }
 
     @AfterMapping
     default void afterMapping(@MappingTarget MapStructEntity4 entity4) {
-        if (StringUtils.isEmpty(entity4.getName())) {
+        if (StringUtils.hasText(entity4.getName())) {
             entity4.setName("whalefall");
         }
         if (entity4.getStatus() == null) {
