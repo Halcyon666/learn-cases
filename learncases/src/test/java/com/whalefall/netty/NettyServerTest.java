8a3415e94005a7654dc2375bc8a801b43d37b381
Halcyon666 <whalefallcl@gmail.com>
2024-06-08 23:56:22 +0800

    add Netty tests
    Signed-off-by: Halcyon666 <whalefallcl@gmail.com>

--- learncases/src/test/java/com/whalefall/netty/NettyServerTest.java
+++ learncases/src/test/java/com/whalefall/netty/NettyServerTest.java
@@ -1,15 +1,16 @@
 package com.whalefall.netty;
 
-import com.whalefall.learncases.netty.client.ClientHandler;
+import com.whalefall.learncases.netty.server.handler.ServerHandler;
 import com.whalefall.learncases.netty.server.protobuf.MessageProtobuf;
-import io.netty.bootstrap.Bootstrap;
+import io.netty.bootstrap.ServerBootstrap;
 import io.netty.channel.*;
 import io.netty.channel.nio.NioEventLoopGroup;
-import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
 import io.netty.handler.codec.LengthFieldBasedFrameDecoder;
 import io.netty.handler.codec.LengthFieldPrepender;
 import io.netty.handler.codec.protobuf.ProtobufDecoder;
 import io.netty.handler.codec.protobuf.ProtobufEncoder;
+import io.netty.handler.timeout.IdleStateHandler;
 import jakarta.annotation.Nullable;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
@@ -18,45 +19,48 @@ import org.springframework.util.Assert;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
-class NettyClientTests {
+class NettyServerTest {
 
-    private static EventLoopGroup group;
+    private static EventLoopGroup bossGroup;
+    private static EventLoopGroup workerGroup;
     private static ChannelFuture channelFuture;
 
     @BeforeAll
-    public static void startClient() throws InterruptedException {
-        group = new NioEventLoopGroup();
+    public static void startServer() throws InterruptedException {
+        bossGroup = new NioEventLoopGroup(1);
+        workerGroup = new NioEventLoopGroup();
 
-        Bootstrap b = new Bootstrap();
-        b.group(group)
-                .channel(NioSocketChannel.class)
-                .option(ChannelOption.TCP_NODELAY, true)
-                .handler(new ChannelInitializer<>() {
+        ServerBootstrap b = new ServerBootstrap();
+        b.group(bossGroup, workerGroup)
+                .channel(NioServerSocketChannel.class)
+                .childHandler(new ChannelInitializer<>() {
                     @Override
                     protected void initChannel(@Nullable Channel ch) {
                         Assert.notNull(ch, "ch must not null");
                         ChannelPipeline p = ch.pipeline();
                         p.addLast("frameEncoder", new LengthFieldPrepender(2));
                         p.addLast(new ProtobufEncoder());
+                        p.addLast(new IdleStateHandler(30, 15, 0));
                         p.addLast("frameDecoder", new LengthFieldBasedFrameDecoder(65535, 0, 2, 0, 2));
                         p.addLast(new ProtobufDecoder(MessageProtobuf.Msg.getDefaultInstance()));
-                        p.addLast(new ClientHandler());
+                        p.addLast(new ServerHandler());
                     }
                 });
-        NettyServerTest.startServer();
-        channelFuture = b.connect("127.0.0.1", 8080).sync();
-        NettyServerTest.stopServer();
+
+        channelFuture = b.bind(8080).sync();
     }
 
     @AfterAll
-    public static void stopClient() {
-        group.shutdownGracefully();
+    public static void stopServer() {
+        bossGroup.shutdownGracefully();
+        workerGroup.shutdownGracefully();
     }
 
     @Test
-    void testClientStart() {
+    void testServerStart() {
         assertNotNull(channelFuture);
         assertNotNull(channelFuture.channel());
     }
 }
+
 
