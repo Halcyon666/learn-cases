5e2dd706752637898f09823818bc1684473849ae
Halcyon666 <whalefallcl@gmail.com>
2024-12-20 23:31:29 +0800

    update MockBean -> MockitoBean
    Signed-off-by: Halcyon666 <whalefallcl@gmail.com>

--- learncases/src/test/java/com/whalefall/AbstractTest.java
+++ learncases/src/test/java/com/whalefall/AbstractTest.java
@@ -5,7 +5,7 @@ import com.whalefall.learncases.netty.server.task.NettyServerTask;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest(classes = LearncasesApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -15,6 +15,6 @@ public abstract class AbstractTest {
     @Autowired
     public MockMvc mockMvc;
 
-    @MockBean
+    @MockitoBean
     public NettyServerTask nettyServerTask;
 }
